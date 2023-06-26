25c0d80799aae4ac3262617e824cda5ae9b974fd
terra456 <terra.456@yandex.ru>
2023-06-27 02:44:30 +0500

    fix: sort files when list it

--- src/fs/list.js
+++ src/fs/list.js
@@ -5,10 +5,19 @@ const list = async (dir) => {
     const sourcePath = path.resolve(dir);
     readdir(sourcePath, {withFileTypes: true})
         .then((files) => {
-            console.table(files.sort((el) => el.isDirectory() ? -1 : 1).map((el) => {
-                return { Name: el.name, Type: el.isDirectory() ? 'directory' : 'file' }
-            }));
-        })
+            console.table(
+                files
+                    .filter((el) => el.isDirectory())
+                    .sort((a, b) => a.name.localeCompare(b.name))
+                    .concat(
+                        files
+                            .filter((el) => !el.isDirectory())
+                            .sort((a, b) => a.name.localeCompare(b.name))
+                    )
+                    .map((el) => {
+                        return { Name: el.name, Type: el.isDirectory() ? 'directory' : 'file' }
+                    }));
+            })
         .catch(() => {
             console.log('FS operation failed: no such folder');
         })
